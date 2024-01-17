806472275fc420aabbafc7eb6476892f15fdb675
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-01-18 03:55:53 +0900

    As of 202401180355

--- hx02/hx02.py
+++ hx02/hx02.py
@@ -10,11 +10,13 @@ from hx02.db import Customer, get_db
 router = APIRouter()
 templates = Jinja2Templates(directory='hx02')
 
+# Ordinal API functions
 @router.get("/")
 async def json_customers(db: Session = Depends(get_db)):
     customers = db.query(Customer).offset(0).limit(100).all()
     return customers
 
+# HTMX Response functions
 @router.get("/list", response_class=HTMLResponse)
 async def list(request: Request, skip: int = 0, limit: int = 1, hx_request: Optional[str] = Header(None), db: Session = Depends(get_db)):
 
